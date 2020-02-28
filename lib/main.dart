490743e1706e6a2be61823e4be83c03d95a8eb63
Luis Enrique Ortiz Ramirez <luisortramirez2211@gmail.com>
2020-02-27 19:57:35 -0600

    Pantalla con estado

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class Aplicacion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: PantallaPrincipal(),
     );
   }
