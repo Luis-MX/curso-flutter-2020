dc2ae148289dbbfdff921a7f904e0bee65f2b61f
Luis Enrique Ortiz Ramirez <luisortramirez2211@gmail.com>
2020-03-03 19:39:18 -0600

    Pantalla con widget stateful optimizado

--- lib/cronometro.dart
+++ lib/cronometro.dart
@@ -60,16 +60,91 @@ class _PantallaCronometroState extends State<PantallaCronometro> {
         child: Text('Patalla optimizada', style: TextStyle(color: Colors.white, fontSize: 24),),
         onPressed: () {
           Navigator.of(context).push(MaterialPageRoute(
-            builder: (_) => Scaffold(
-              appBar: AppBar(
-                title: Text('Pantalla optimizada'),
-              ),
-              body: Center(
-                child: TextField(),
-              ),
-            )
+            builder: (_) => PantallaOptimizada()
           ));
         },
+      ),
+    );
+  }
+}
+
+class PantallaOptimizada extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('construyendo pantalla');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pantalla Optimizada'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CronometroWidget(),
+            SizedBox(height: 24,),
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                //
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CronometroWidget extends StatefulWidget {
+  @override
+  _CronometroWidgetState createState() => _CronometroWidgetState();
+}
+
+class _CronometroWidgetState extends State<CronometroWidget> {
+
+  int segundos = 0;
+  StreamSubscription _cronometro;
+
+  @override
+  void initState() {
+    super.initState();
+    _cronometro = Stream.periodic(Duration(seconds: 1), actualizacion)
+      .listen(null);
+  }
+
+  @override
+  void dispose() {
+    _cronometro.cancel();
+    super.dispose();
+  }
+
+  void actualizacion(int index) {
+    setState(() {
+      segundos ++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    int minutos = segundos ~/ 60;
+    int segundosPantalla = segundos % 60;
+    print('construyendo cronometro');
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          segundos = 0;
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+          color: Colors.indigo,
+        ),
+        child: Text(
+          '${minutos.toString().padLeft(2, "0")}:${segundosPantalla.toString().padLeft(2, "0")}', 
+          style: TextStyle(fontSize: 32, color: Colors.white),
+          ),
       ),
     );
   }
